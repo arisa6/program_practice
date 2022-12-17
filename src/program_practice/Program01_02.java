e27fca844f8ea8f3962a13bb31fbd2b3a9cca6d1
arisa6 <arisa.masunaga6@gmail.com>
2022-12-18 02:58:48 +0900

    Program01_02

---
+++ src/program_practice/Program01_02.java
@@ -0,0 +1,16 @@
+package program_practice;
+
+public class Program01_02 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		// 1から10までの和を求めるプログラムを作成せよ。
+		// ただし、for文を用いること。
+		int sum = 0;
+		for (int i = 0; i <= 10; i++) {
+			sum += i;
+		}
+		System.out.println(sum);
+	}
+
+}
