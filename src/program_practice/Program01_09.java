189d0090dfe8417f0a8b21fbbcd34d67b6de74a7
arisa6 <arisa.masunaga6@gmail.com>
2022-12-18 23:00:03 +0900

    add: Program01_09

---
+++ src/program_practice/Program01_09.java
@@ -0,0 +1,15 @@
+package program_practice;
+
+public class Program01_09 {
+
+	public static void main(String[] args) {
+		// int型の変数nの数だけ縦横に●が表示されるプログラムを作成せよ。
+		int n = 5;
+		for (int i = 0; i < n; i++) {		// 縦
+			for (int j = 0; j < n; j++) {		//横
+			}
+			System.out.println("●");
+		}
+		System.out.println();
+	}
+}
