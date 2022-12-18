b3af7fb84af197f561218409214427d878d971e0
arisa6 <arisa.masunaga6@gmail.com>
2022-12-18 21:29:20 +0900

    add: Program01_08

---
+++ src/program_practice/Program01_08.java
@@ -0,0 +1,13 @@
+package program_practice;
+
+public class Program01_08 {
+
+	public static void main(String[] args) {
+		// int型の変数xとyの差を表示するプログラムを作成せよ。
+		int x = 20;
+		int y = 4;
+		
+		System.out.println(Math.abs(x - y));
+	}
+
+}
