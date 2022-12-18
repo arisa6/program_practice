2bc1c35f05afe025f51c57997b14295928a235b1
arisa6 <arisa.masunaga6@gmail.com>
2022-12-18 17:43:04 +0900

    add: Program01_04

--- src/program_practice/Program01_04.java
+++ src/program_practice/Program01_04.java
@@ -3,8 +3,18 @@ package program_practice;
 public class Program01_04 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		// int型の変数nが0まで1ずつ増減するプログラムを作成せよ。
+		// nが正の場合は減り、負の場合は増えていくこと。
+		int n = 5;
+		if (n > 0) {
+			for (int i = n; i >= 0; i-- ) {
+				System.out.println(i);
+			}
+		} else {
+			for (int i = n; i <= 0; i++) {
+				System.out.println(i);
+			}
+		}
 	}
 
 }
